682bbdc5c524b2b9bcb0035c8a04909994fdaebd
Underordhi <Matt15hart@gmail.com>
2026-05-09 15:41:54 +0100

    made struct in header file
    struct made in header file,

---
+++ waveform.h
@@ -0,0 +1,15 @@
+#ifndef waveform_h
+#define waveform_h
+
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} waveform;
+
+#endif
